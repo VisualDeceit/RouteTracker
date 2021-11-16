3b21715a4e422354ccc1e944a93505c0243ea250
Alexander Fomin <inzhener@outlook.com>
2021-11-16 19:20:24 +0300

    Renaming

--- RouteTracker/Extensions/UIViewController+nib.swift
+++ RouteTracker/Extensions/UIViewController+nib.swift
@@ -1,5 +1,5 @@
 //
-//  UIViewControllel+nib.swift
+//  UIViewController+nib.swift
 //  RouteTracker
 //
 //  Created by Александр Фомин on 13.11.2021.
@@ -8,6 +8,8 @@
 import UIKit
 
 extension UIViewController {
+    /// Загрузка UIView из *.xib файла
+    /// - Parameter nibName: Имя  файла с раcширением *.xib
    func loadFromNibNamed(nibName: String) -> UIView? {
         let bundle = Bundle(for: type(of: self))
         let nib = UINib(nibName: nibName, bundle: bundle)
